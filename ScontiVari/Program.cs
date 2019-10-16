f7a6f5de5cbedc9b66d0d66d3962cee86f9d48cd
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:40:08 +0200

    finito

---
+++ ScontiVari/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ScontiVari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quanto hai speso?");
+            double importo = double.Parse(Console.ReadLine());
+            if (importo<=500)
+            {
+                double sconto1 = (importo * 10) / 100;
+                double finale1 = importo - sconto1;
+                Console.WriteLine($"lo sconto è di {sconto1:c} e l'imprto finale è di {finale1:c}");
+            }
+            if (importo>500)
+            {
+                double sconto2 = (importo * 20) / 100;
+                double finale2 = importo - sconto2;
+                Console.WriteLine($"lo sconto è di {sconto2:c} e l'imprto finale è di {finale2:c}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
